f352d62144b6c3d60446a29507930c853a69c222
Wallace <wallacesb@outlook.com.br>
2023-04-20 20:50:13 -0300

    Adicionando getAll para Postagem e Tema

--- src/components/postagens/listaPostagens/ListaPostagens.tsx
+++ src/components/postagens/listaPostagens/ListaPostagens.tsx
@@ -1,30 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ListaPostagens.css'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Postagem } from '../../../models/Postagem';
+import useLocalStorage from 'react-use-localstorage';
+import { useNavigate } from 'react-router-dom';
+import { getAll } from '../../../services/Service';
 
 function ListaPostagens() {
+    const [postagens, setPostagens] = useState<Postagem[]>([])
+    const [token] = useLocalStorage('token')
+    const history = useNavigate()
+
+    useEffect(() => {
+        if (token === '') {
+            alert('faça login para acessar essa página')
+            history('/login')
+        }
+    }, [token])
+
+    async function getAllPostagens() {
+        await getAll('/postagens', setPostagens, {
+            headers: {
+                Authorization: token
+            }
+        })
+    }
+
+    useEffect(() => {
+        getAllPostagens()
+    }, [])
+
     return (
-        <Card sx={{ minWidth: 275 }} style={{ margin: '1% 2%', background: 'var(--tea-rose-red)', color: 'white'}}>
-            <CardContent>
-                <Typography variant="h5" component="div">
-                    Título
-                </Typography>
-                <Typography sx={{ mb: 1.5 }}>
-                    Texto da postagem
-                </Typography>
-                <Typography variant="body2">
-                    Tema
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button variant='contained' size="small">editar</Button>
-                <Button style={{color: 'red'}} color='error' variant='outlined' size="small">deletar</Button>
-            </CardActions>
-        </Card >
+        <>
+            {
+                postagens.map((postagem) => (
+                    <Card sx={{ minWidth: 275 }} style={{ margin: '1% 2%', background: 'var(--tea-rose-red)', color: 'white' }}>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                {postagem.titulo}
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }}>
+                                {postagem.texto}
+                            </Typography>
+                            <Typography variant="body2">
+                                {postagem.tema?.descricao}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button variant='contained' size="small">editar</Button>
+                            <Button style={{ color: 'red' }} color='error' variant='outlined' size="small">deletar</Button>
+                        </CardActions>
+                    </Card >
+                ))
+            }
+            {console.log(postagens)}
+        </>
     )
 }
 
--- src/components/temas/listaTemas/ListaTemas.tsx
+++ src/components/temas/listaTemas/ListaTemas.tsx
@@ -1,27 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ListaTemas.css'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Tema } from '../../../models/Tema';
+import useLocalStorage from 'react-use-localstorage';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAll } from '../../../services/Service';
 
 function ListaTemas() {
+    const [temas, setTemas] = useState<Tema[]>([])
+    const [token] = useLocalStorage('token')
+    const history = useNavigate()
+
+    useEffect(() => {
+        if (token === '') {
+            alert('faça login para acessar essa página')
+            history('/login')
+        }
+    }, [token])
+
+    async function getAllTemas() {
+        await getAll('/temas', setTemas, {
+            headers: {
+                Authorization: token
+            }
+        })
+    }
+
+    useEffect(() => {
+        getAllTemas()
+    }, [temas.length])
+
     return (
-        <Card sx={{ minWidth: 275 }} style={{ margin: '1% 2%' }}>
-            <CardContent>
-                <Typography sx={{ fontSize: 14 }} gutterBottom>
-                    tema
-                </Typography>
-                <Typography variant="h5" component="div">
-                    título do Tema
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button variant='contained' size="small">editar</Button>
-                <Button variant='outlined' style={{color: 'red'}} color='error' size="small">deletar</Button>
-            </CardActions>
-        </Card >
+        <>
+            {
+                temas.map((tema) => (
+                    <Card sx={{ minWidth: 275 }} style={{ margin: '1% 2%' }}>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} gutterBottom>
+                                tema
+                            </Typography>
+                            <Typography variant="h5" component="div">
+                                {tema.descricao}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Link to={`/atualizar-tema/${tema.id}`}>
+                                <Button variant='contained' size="small">editar</Button>
+                            </Link>
+                            <Link to={`/deletar-tema/${tema.id}`}>
+                                <Button variant='outlined' style={{ color: 'red' }} color='error' size="small">deletar</Button>
+                            </Link>
+                        </CardActions>
+                    </Card >
+                ))
+            }
+        </>
     )
 }
 
