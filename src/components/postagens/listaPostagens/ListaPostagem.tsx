005c67f88c698beba481ef67ec4701976867837d
Wallace <wallacesb@outlook.com.br>
2023-04-20 21:03:16 -0300

    Organizando funcoes de ListaPostagem

---
+++ src/components/postagens/listaPostagens/ListaPostagem.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import './ListaPostagens.css'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Postagem } from '../../../models/Postagem';
+import useLocalStorage from 'react-use-localstorage';
+import { useNavigate } from 'react-router-dom';
+import { getAll } from '../../../services/Service';
+
+function ListaPostagem() {
+    const [postagens, setPostagens] = useState<Postagem[]>([])
+    const [token] = useLocalStorage('token')
+    const history = useNavigate()
+
+    useEffect(() => {
+        if (token === '') {
+            alert('faça login para acessar essa página')
+            history('/login')
+        }
+    }, [token])
+
+    useEffect(() => {
+        getAllPostagens()
+    }, [])
+
+    async function getAllPostagens() {
+        await getAll('/postagens', setPostagens, {
+            headers: {
+                Authorization: token
+            }
+        })
+    }
+
+    return (
+        <>
+            {
+                postagens.map((postagem) => (
+                    <Card sx={{ minWidth: 275 }} style={{ margin: '1% 2%', background: 'var(--tea-rose-red)', color: 'white' }}>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                {postagem.titulo}
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }}>
+                                {postagem.texto}
+                            </Typography>
+                            <Typography variant="body2">
+                                {postagem.tema?.descricao}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button variant='contained' size="small">editar</Button>
+                            <Button style={{ color: 'red' }} color='error' variant='outlined' size="small">deletar</Button>
+                        </CardActions>
+                    </Card >
+                ))
+            }
+            {console.log(postagens)}
+        </>
+    )
+}
+
+export default ListaPostagem
